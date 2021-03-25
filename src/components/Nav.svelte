cc020e7a3de38bd006a4df98ef06c052117f29c2
Yerassyl Maikhanov <maihanov96@gmail.com>
2021-03-25 17:44:36 +0600

    changed styles

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,22 +3,23 @@
 </script>
 
 <nav>
-
     <ul class="list-group">
-        <li class="list-group-item active" aria-current="true">An active item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+        <a aria-current="{segment === undefined ? 'page' : undefined}" href=".">
+            <li class="list-group-item">home</li>
+        </a>
+        <a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">
+            <li class="list-group-item">about</li>
+        </a>
+
+        <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+             the blog data when we hover over the link or tap it on a touchscreen -->
+        <a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}"
+           href="blog">
+            <li class="list-group-item">blog</li>
+        </a>
+        <a rel=prefetch aria-current="{segment === 'login' ? 'page' : undefined}"
+           href="login">
+            <li class="list-group-item">login</li>
+        </a>
     </ul>
-
-    <!--	<ul>-->
-    <!--		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>-->
-    <!--		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>-->
-
-    <!--		&lt;!&ndash; for the blog link, we're using rel=prefetch so that Sapper prefetches-->
-    <!--		     the blog data when we hover over the link or tap it on a touchscreen &ndash;&gt;-->
-    <!--		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>-->
-    <!--		<li><a rel=prefetch aria-current="{segment === 'login' ? 'page' : undefined}" href="login">login</a></li>-->
-    <!--	</ul>-->
 </nav>
